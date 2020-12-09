35ff70dc1a0ced68dd2734e8825361433593dc6c
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2020-12-09 09:31:43 +0530

    deleted comment line

--- src/Components/CardExample.jsx
+++ src/Components/CardExample.jsx
@@ -20,7 +20,6 @@ export const CardExample = () => {
   );
 };
 
-//             <Card.Title>OnClick Hideing a Card</Card.Title>
 //             <Card.Text>
 //               Some quick example text to build on the card title and make up the
 //               bulk of the card's content Some quick example text to build on the
