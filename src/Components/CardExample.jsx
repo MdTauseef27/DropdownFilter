84c8c518c91592173d44160798fbe3a94b8efe87
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2020-12-10 09:30:19 +0530

    deleted comment data

--- src/Components/CardExample.jsx
+++ src/Components/CardExample.jsx
@@ -26,11 +26,6 @@ export const CardExample = () => {
   );
 };
 
-<Card.Text>
-  Some quick example text to build on the card title and make up the // bulk of
-  the card's content Some quick example text to build on the // card title and
-  make up the bulk of the card's content.
-</Card.Text>;
 //           </Card.Body>
 //         </Card>
 //       )}
