3658c310e7ccfbcf41778e0907c2498203fd00de
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2020-12-11 09:42:27 +0530

    toggle on click of button

--- src/Components/CardExample.jsx
+++ src/Components/CardExample.jsx
@@ -6,22 +6,24 @@ export const CardExample = () => {
   const [isShow, setISShow] = useState("true");
   return (
     <div>
-      <Card style={{ width: "300px", height: "300px" }}>
-        <Card.Img
-          style={{ width: "100px", height: "100px" }}
-          src={imgLogo}
-          alt=""
-        ></Card.Img>
-        <Card.Body>
-          <Card.Title>Click To Hide</Card.Title>
-          <Card.Text>
-            {" "}
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content Some quick example text to build on the
-            card title and make up the bulk of the card's content.
-          </Card.Text>
-        </Card.Body>
-      </Card>
+      {isShow && (
+        <Card style={{ width: "300px", height: "300px" }}>
+          <Card.Img
+            style={{ width: "100px", height: "100px" }}
+            src={imgLogo}
+            alt=""
+          ></Card.Img>
+          <Card.Body>
+            <Card.Title>Click To Hide</Card.Title>
+            <Card.Text>
+              {" "}
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content Some quick example text to build on the
+              card title and make up the bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      )}
       <br></br>
       <Button variant="primary" onClick={() => setISShow(!isShow)}>
         Click ME
