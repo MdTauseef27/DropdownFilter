802986359e93c45109bc185d1ff14c37cebf2a69
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2020-12-24 09:17:55 +0530

    changes

--- src/Components/FilterData.jsx
+++ src/Components/FilterData.jsx
@@ -34,7 +34,6 @@ export const FilterData = () => {
   );
 };
 
-//         .filter((person) => person.age > 60)
 //         .map((filteredPerson) => (
 //           <li>
 //             {filteredPerson.name}
