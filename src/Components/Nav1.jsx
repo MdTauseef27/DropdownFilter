77c8e987a8af223b99407cca012244a4c7f0613d
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2021-01-08 09:57:31 +0530

    implemented nav bar

--- src/Components/Nav1.jsx
+++ src/Components/Nav1.jsx
@@ -1,33 +1,48 @@
 import React from "react";
+import { Nav, Form, FormControl, Button, Navbar } from "react-bootstrap";
 
 export const Nav1 = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <img
-            className="logo"
-            src="/ProductImages/The-Plug-Logo.jpg"
-            alt="The Plug Logo"
-          />
-        </li>
-        <li>
-          <a className="navbutton" href="home.html">
-            Home
-          </a>
-        </li>
-        <li className="active">
-          <a class="navbutton" href="products.html">
-            Products
-          </a>
-        </li>
-        <li>
-          <a className="navbutton" href="contact.html">
-            Contact
-          </a>
-        </li>
-      </ul>
-    </nav>
+    <div>
+      {/* <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">Shopping</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#products">Products</Nav.Link>
+          <Nav.Link href="#contacts">Contacts</Nav.Link>
+        </Nav>
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-info">Search</Button>
+        </Form>
+      </Navbar> */}
+    </div>
+    // <nav>
+    //   <ul>
+    //     <li>
+    //       <img
+    //         className="logo"
+    //         src="/ProductImages/The-Plug-Logo.jpg"
+    //         alt="The Plug Logo"
+    //       />
+    //     </li>
+    //     <li>
+    //       <a className="navbutton" href="home.html">
+    //         Home
+    //       </a>
+    //     </li>
+    //     <li className="active">
+    //       <a class="navbutton" href="products.html">
+    //         Products
+    //       </a>
+    //     </li>
+    //     <li>
+    //       <a className="navbutton" href="contact.html">
+    //         Contact
+    //       </a>
+    //     </li>
+    //   </ul>
+    // </nav>
   );
 };
 
