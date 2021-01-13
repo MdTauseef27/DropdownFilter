4e1ec4cbc1c62478840fdaf9979af461fbeafa4f
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2021-01-13 09:42:55 +0530

    created parent component of lifting

---
+++ src/Components/Lifting.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import App from "../App";
+import { LiftingChild } from "./LiftingChild";
+export const Lifting = ({ Count, decrement, increment }) => {
+  // const [isCount, setIsCount] = useState(0);
+  // const increment = () => {
+  //   setIsCount(isCount + 1);
+  // };
+  // const decrement = () => {
+  //   setIsCount(isCount - 1);
+  // };
+  const [isName, setIsName] = useState("Mohammed Tauseef");
+  return (
+    <div>
+      <h1>hiiiiiiiiiii</h1>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <p>Increment:{Count}</p>
+      {/* {isName} */}
+      <LiftingChild isName={isName} />
+    </div>
+  );
+};
