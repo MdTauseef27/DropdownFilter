1cfb266e3e88f40567833f405d4bfca7329b1223
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2021-01-13 09:43:33 +0530

    calling component in app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { DropdownFilter } from "./Components/DropdownFilter";
 import { CardExample } from "./Components/CardExample";
@@ -9,8 +9,16 @@ import { FilteredName } from "./Components/FilteredName";
 import { Products1 } from "./Components/Products1";
 import { Nav1 } from "./Components/Nav1";
 import { Footer1 } from "./Components/Footer1";
-
+import { Lifting } from "./Components/Lifting";
+import { ItemsScrollBar } from "./Components/ItemsScrollBar";
 function App() {
+  // const [isCount, setIsCount] = useState(0);
+  // const increment = () => {
+  //   setIsCount(isCount + 1);
+  // };
+  // const decrement = () => {
+  //   setIsCount(isCount - 1);
+  // };
   return (
     <div>
       {/* <DropdownFilter /> */}
@@ -18,9 +26,11 @@ function App() {
       {/* <FilterData /> */}
       {/* <FilterDataEx /> */}
       {/* <FilteredName /> */}
-      <Nav1 />
-      <Products1 />
+      {/* <Nav1 /> */}
+      {/* <Products1 /> */}
       {/* <Footer1 /> */}
+      {/* <Lifting Count={isCount} decrement={decrement} increment={increment} /> */}
+      <ItemsScrollBar />
     </div>
   );
 }
