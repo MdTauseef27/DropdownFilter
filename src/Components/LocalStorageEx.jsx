931fa878ff8ef55b010b1add9c073cab4fd5ec4e
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2021-01-14 17:27:47 +0530

    getting the value from local storage

--- src/Components/LocalStorageEx.jsx
+++ src/Components/LocalStorageEx.jsx
@@ -1,9 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Nav } from "react-bootstrap";
 export const LocalStorageEx = () => {
-  const [value, setValue] = useState();
+  const [value, setValue] = useState(
+    "HIII",
+    localStorage.getItem("myValueInLocalStorage") || ""
+  );
+  useEffect(() => {
+    localStorage.setItem("myValueInLocalStorage", value);
+  }, [value]);
+
+  //   const on = (event) => setValue(event.target.value);
   return (
     <div>
       <h1>Hello React with Local Storage!</h1>
+      {/* <input value={value} type="text" onChange={onChange} /> */}
+      <Nav.Link onClick={(e) => setValue(e.target.value)}>
+        Corporate Treasury
+      </Nav.Link>
+
+      <p>{value}</p>
     </div>
   );
 };
