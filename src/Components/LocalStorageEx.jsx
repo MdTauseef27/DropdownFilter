7e93f6cee1160321dc9c2564084867b43a8d585e
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2021-01-14 16:19:16 +0530

    added h1 tag

--- src/Components/LocalStorageEx.jsx
+++ src/Components/LocalStorageEx.jsx
@@ -1,5 +1,9 @@
 import React, { useState } from "react";
 export const LocalStorageEx = () => {
   const [value, setValue] = useState();
-  return <div></div>;
+  return (
+    <div>
+      <h1>Hello React with Local Storage!</h1>
+    </div>
+  );
 };
