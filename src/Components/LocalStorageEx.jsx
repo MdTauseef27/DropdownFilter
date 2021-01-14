b74c18e8fe1d1248030e4162e570458f3079056f
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2021-01-14 16:15:21 +0530

    export component

--- src/Components/LocalStorageEx.jsx
+++ src/Components/LocalStorageEx.jsx
@@ -1,10 +1,8 @@
 import React from "react";
-const LocalStorageEx = () => {
+export const LocalStorageEx = () => {
   return (
     <div>
       <h1>Hiiiiiiiiiii</h1>
     </div>
   );
 };
-
-export default LocalStorageEx;
