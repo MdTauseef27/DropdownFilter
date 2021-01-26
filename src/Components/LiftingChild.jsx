c46681bce328cf874d48e1ae70559ebf5bafda20
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2021-01-26 11:20:26 +0530

    deleted button on liftingchild component

--- src/Components/LiftingChild.jsx
+++ src/Components/LiftingChild.jsx
@@ -3,9 +3,6 @@ export const LiftingChild = ({ isName, increments, decrements, isCount }) => {
   return (
     <div>
       <h1>{isName}</h1>
-      <button onClick={increments}>Increments</button>
-      <button onClick={decrements}>Decrements</button>
-      <p>Increment:{isCount}</p>
     </div>
   );
 };
