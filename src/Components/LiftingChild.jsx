603f62660145df3e00d5c83fac4efceb7d9ae6bb
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2021-01-26 11:15:26 +0530

    lifting up the state

--- src/Components/LiftingChild.jsx
+++ src/Components/LiftingChild.jsx
@@ -1,8 +1,11 @@
 import React from "react";
-export const LiftingChild = ({ isName }) => {
+export const LiftingChild = ({ isName, increments, decrements, isCount }) => {
   return (
     <div>
       <h1>{isName}</h1>
+      <button onClick={increments}>Increments</button>
+      <button onClick={decrements}>Decrements</button>
+      <p>Increment:{isCount}</p>
     </div>
   );
 };
