7824adee45ede7ba8d2bae49ede97a47903ea65f
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2021-02-22 10:42:09 +0530

    changes in the component

--- src/Components/LiftingChild.jsx
+++ src/Components/LiftingChild.jsx
@@ -2,7 +2,7 @@ import React from "react";
 export const LiftingChild = ({ isName, increment, decrement, Count }) => {
   return (
     <div>
-      <h1>{isName}</h1>
+      <h1>Name:{isName}</h1>
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
       <p>Counts:{Count}</p>
