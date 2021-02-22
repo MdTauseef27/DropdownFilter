804fc31e51e5e2dd1f1778bc3b3074e2f36522dc
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2021-02-22 10:41:16 +0530

    changes

--- src/Components/LiftingChild.jsx
+++ src/Components/LiftingChild.jsx
@@ -5,7 +5,7 @@ export const LiftingChild = ({ isName, increment, decrement, Count }) => {
       <h1>{isName}</h1>
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
-      <p>Increment:{Count}</p>
+      <p>Counts:{Count}</p>
     </div>
   );
 };
