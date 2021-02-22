b1f2d2cc8dfe106a2064ac8d162cafa33bb3772e
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2021-02-22 10:42:58 +0530

    deleted h1 tag and added p tag

--- src/Components/LiftingChild.jsx
+++ src/Components/LiftingChild.jsx
@@ -2,7 +2,7 @@ import React from "react";
 export const LiftingChild = ({ isName, increment, decrement, Count }) => {
   return (
     <div>
-      <h1>Name:{isName}</h1>
+      <p>Name:{isName}</p>
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
       <p>Counts:{Count}</p>
