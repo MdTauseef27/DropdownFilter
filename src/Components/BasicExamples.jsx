c0721ac8d937cdb6500adbe022c8642b3f4ff503
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2021-10-07 11:47:11 +0530

    added two buttons fo increment and decrement

--- src/Components/BasicExamples.jsx
+++ src/Components/BasicExamples.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
 export const BasicExamples = () => {
+  const [isCount, isShowCount] = useState(0);
   return (
     <div>
       <h1 style={{ textAlign: "center", color: "red" }}>
         Well Come To React World
       </h1>
+      <Button style={{ marginLeft: "20px" }}>Icrement</Button>
+      <Button style={{ marginLeft: "20px" }}>Decrement</Button>
     </div>
   );
 };
