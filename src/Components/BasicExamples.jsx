13e6e1187846ddc865eeeca38b454515e004cc83
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2021-10-07 12:17:00 +0530

    changes

--- src/Components/BasicExamples.jsx
+++ src/Components/BasicExamples.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Button } from "react-bootstrap";
+import { Button, InputGroup, FormControl } from "react-bootstrap";
 export const BasicExamples = () => {
   const [isCount, isShowCount] = useState(0);
   return (
@@ -7,8 +7,18 @@ export const BasicExamples = () => {
       <h1 style={{ textAlign: "center", color: "red" }}>
         Well Come To React World
       </h1>
-      <Button style={{ marginLeft: "20px" }}>Icrement</Button>
+      <Button
+        style={{ marginLeft: "20px" }}
+        onClick={() => isShowCount(isCount + 1)}
+      >
+        Icrement
+      </Button>
       <Button style={{ marginLeft: "20px" }}>Decrement</Button>
+      <InputGroup className="mb-3">
+        <InputGroup.Text>First and last name</InputGroup.Text>
+        <FormControl aria-label="First name" />
+        <FormControl aria-label="Last name" />
+      </InputGroup>
     </div>
   );
 };
