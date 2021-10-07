47497338cbbb84af463a515b20f1b0d48ecbf81f
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2021-10-07 11:42:47 +0530

    provide some style

--- src/Components/BasicExamples.jsx
+++ src/Components/BasicExamples.jsx
@@ -2,7 +2,9 @@ import React from "react";
 export const BasicExamples = () => {
   return (
     <div>
-      <h1>Well Come To React World</h1>
+      <h1 style={{ textAlign: "center", color: "red" }}>
+        Well Come To React World
+      </h1>
     </div>
   );
 };
